8c82ed850d2eb706bc1894fdb2e4505f69fbb996
jinseo0904 <jinseokim@ist2-01635.apn.wlan.private.upenn.edu>
2024-03-19 12:33:49 -0400

    added backend connection to MongoDB

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://cis3500:Bpe9H44mfrKYCcFA@dev.og2wrsm.mongodb.net/?retryWrites=true&w=majority&appName=Dev';
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// for login APIs
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Middleware fror handling CORS policy
+app.use(cors());
+
+
+//generating routes
+app.get('/', (request, response) => {
+    //console.log(request);
+    return response.status(234).send("Successfully Connected to CIS 3500 Group 5 - Buy and Sell");
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
