ac2813f94594c79bea9301e20d9c3347e4aaf3d4
jinseo0904 <jinseokim@gimjinscbookpro.mynetworksettings.com>
2024-03-20 19:51:16 -0400

    added front end using vite

---
+++ backend/models/userModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+
+const userSchema = mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+
+        lastName: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: true,
+            validate: {
+              validator: (value) => {
+                const eduEmailRegex = /^.+@.+\.edu$/; // Matches email ending with ".edu"
+                return eduEmailRegex.test(value);
+              },
+              message: 'A valid school email ending with ".edu" must be used.',
+            },
+          },
+        password: {
+            type: String,
+            minlength: 5,
+            required: true,
+        },
+        role: {
+            type: Number,
+            default: 0, // 0 for normal user, 1 for admin
+        },
+        token: {
+            type: String,
+        },
+        tokenExp: {
+            type: Number,
+        },
+
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const User = mongoose.model('User', userSchema);
