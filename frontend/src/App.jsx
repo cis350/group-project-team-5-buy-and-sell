6d1d97cf8c2e85831104db299e0f0f5c882630fd
jinseo0904 <jinseokim@gimjinscbookpro.mynetworksettings.com>
2024-03-26 01:31:43 -0400

    added eslint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import { AnimatePresence } from "framer-motion";
-import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <AnimatePresence>
       <Routes>
-      <Route path ='/' element={<Home />}/>
-      <Route path ='/login' element={<LoginPage />}/>
-   </Routes>
-      </AnimatePresence>
-  ) 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-  // Adding Custom Colors: Define Custom Colors Here 
+      // Adding Custom Colors: Define Custom Colors Here
       colors: {
-        'penn-blue':'#011F5B',
+        'penn-blue': '#011F5B',
       },
       // Define Custom Fonts Here
       fontFamily: {
-        interbold: ["InterBold", "sans-serif"],
-        interextra: ["InterExtraBold", "sans-serif"],
-        inter: ["InterMedium", "sans-serif"],
-        interlight: ["InterLight", "sans-serif"],
+        interbold: ['InterBold', 'sans-serif'],
+        interextra: ['InterExtraBold', 'sans-serif'],
+        inter: ['InterMedium', 'sans-serif'],
+        interlight: ['InterLight', 'sans-serif'],
       },
       spacing: {
-        '12': '48px', // Add a custom value '7.5' that equals '30px'
+        12: '48px', // Add a custom value '7.5' that equals '30px'
       },
     },
   },
   plugins: [],
-}
-
+};
