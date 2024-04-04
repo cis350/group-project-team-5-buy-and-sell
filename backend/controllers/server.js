7541f8898d1229a4352fd52f501c4073ebbbbf6c
jinseo0904 <jinseokim@gimjinscbookpro.mynetworksettings.com>
2024-04-03 21:17:58 -0400

    debug server side code

--- backend/controllers/server.js
+++ backend/controllers/server.js
@@ -14,27 +14,14 @@ const webapp = express();
 webapp.use(cookieParser());
 
 // Enable CORS and body parsing
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://group-project-team-5-buy-and-sell.vercel.app',
-];
-
 webapp.use(cors({
-    origin(origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: ['http://localhost:5173', 'https://group-project-team-5-buy-and-sell.vercel.app'],
     credentials: true,
 }));
+
 webapp.use(bodyParser.urlencoded({ extended: false }));
 webapp.use(bodyParser.json()); // Support JSON encoded bodies
 
-console.log(process.env.NODE_ENV);
 // Session configuration
 webapp.use(session({
     secret: 'real secret key',
