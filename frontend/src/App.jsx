cd32e329687bdc57f2251cc4de3c0104159d115b
jinseo0904 <jinseokim@ist-02864.apn.wlan.private.upenn.edu>
2024-04-05 12:18:16 -0400

    basic setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { SnackbarProvider } from 'notistack';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
+import AddItemPage from './pages/AddItemPage';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="/additem" element={<AddItemPage />} />
         </Routes>
       </AnimatePresence>
     </SnackbarProvider>
